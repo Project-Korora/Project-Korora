8b39f5aa3584b03aeaa5b879b9fe82bd2755538c
piperinnshall <piper.innshall@gmail.com>
2025-04-02 14:46:39 +1300

    style: add Flake8 configuration file

--- Project_Korora/src/main.py
+++
@@ -1,5 +0,0 @@
-def main():
-    print("Project Kororā")
-
-if __name__ == "__main__":
-    main()
--- Project_Korora/tests/test_main.py
+++
@@ -1,6 +0,0 @@
-from Project_Korora.src.main import main
-
-def test_main_output(capsys):
-    main()
-    captured = capsys.readouterr()
-    assert "Project Kororā" in captured.out
