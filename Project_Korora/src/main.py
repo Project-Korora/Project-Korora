6e0e3e6794fc890a71412d301085220e85dad0fb
Rm91bmQgbWUh <211602380+Rm91bmQgbWUh@users.noreply.github.com>
2026-03-20 14:59:24 +1300

    feat(code transfer): Transfer main.py code from https://github.com/pycubed/software_example_beepsat/blob/main/advanced/main.py as it was missed in previous transfer.

--- Project_Korora/src/main.py
+++ Project_Korora/src/main.py
@@ -1,6 +1,62 @@
-def main():
-    print("Project Kororā")
+import os
+import traceback
+from time import sleep
 
+import tasko
+from pycubed import cubesat
 
-if __name__ == "__main__":
-    main()
+print("Initializing PyCubed Hardware...")
+
+# create asyncio object
+cubesat.tasko = tasko
+# Dict to store scheduled objects by name
+cubesat.scheduled_tasks = {}
+
+print("Loading Tasks...", end="")
+# schedule all tasks in directory
+for file in os.listdir("Tasks"):
+    # remove the '.py' from file name
+    file = file[:-3]
+
+    # ignore these files
+    if file in ("template_task", "test_task", "listen_task") or file.startswith("._"):
+        continue
+
+    # auto-magically import the task file
+    exec(f"import Tasks.{file}")
+    # create a helper object for scheduling the task
+    task_obj = eval("Tasks." + file).task(cubesat)
+
+    # determine if the task wishes to be scheduled later
+    if hasattr(task_obj, "schedule_later") and getattr(task_obj, "schedule_later"):
+        schedule = cubesat.tasko.schedule_later
+    else:
+        schedule = cubesat.tasko.schedule
+
+    # schedule each task object and add it to our dict
+    cubesat.scheduled_tasks[task_obj.name] = schedule(
+        task_obj.frequency, task_obj.main_task, task_obj.priority
+    )
+print(len(cubesat.scheduled_tasks), "total")
+
+print("Running...")
+try:
+    # should run forever
+    cubesat.tasko.run()
+except Exception as e:
+    formatted_exception = traceback.format_exception(e, e, e.__traceback__)
+    print(formatted_exception)
+    try:
+        # increment our NVM error counter
+        cubesat.c_state_err += 1
+        # try to log everything
+        cubesat.log(f"{formatted_exception},{cubesat.c_state_err},{cubesat.c_boot}")
+    except Exception:
+        pass
+
+# we shouldn't be here!
+print("Engaging fail safe: hard reset")
+
+sleep(10)
+cubesat.micro.on_next_reset(cubesat.micro.RunMode.NORMAL)
+cubesat.micro.reset()
