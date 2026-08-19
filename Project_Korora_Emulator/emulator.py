1547139eb55218b5320b59864463c8984a6dfc1d
Nick <nicholasakho@gmail.com>
2026-08-19 17:31:29 +1200

    feat: Parser requires both config and data arguments for a given scenario

--- Project_Korora_Emulator/emulator.py
+++ Project_Korora_Emulator/emulator.py
@@ -1,2 +1,26 @@
 import argparse
+import os
+import sys
+from pathlib import Path
 
+def main() -> int:
+    """Emulate a satellite scenario. Returns an exit status."""
+    parser = argparse.ArgumentParser(
+        description="Emulate a scenario."
+    )
+
+    # Retrieve configuration data for the scenario, from a directory (path)
+    parser.add_argument(
+        "config_dir",
+        type=Path,
+        help="Directory containing config files for the scenario."
+    )
+
+    # Retrieve scenario data from a directory.
+    parser.add_argument(
+        "data_dir",
+        type=Path,
+        help="Directory that contains the data for the scenario."
+    )
+
+    args = parser.parse_args()
