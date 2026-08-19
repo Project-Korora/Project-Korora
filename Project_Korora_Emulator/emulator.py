feebeb7b8a2fe1f1640b18402c249685582e4c57
Nick <nicholasakho@gmail.com>
2026-08-19 17:38:48 +1200

    feat: Parser turns all given directory arguments into absolute paths + verifies them

--- Project_Korora_Emulator/emulator.py
+++ Project_Korora_Emulator/emulator.py
@@ -24,3 +24,30 @@ def main() -> int:
     )
 
     args = parser.parse_args()
+
+    # Convert to absolute file path
+    emulator_directory = Path(__file__).resolve().parent
+
+    project_root = emulator_directory.parent
+    satellite_main = project_root / "Project_Korora" / "src" / "main.py"
+
+    emulator_lib = emulator_directory / "lib"
+
+    # Turn given arguments into absolute paths
+    config_directory = args.config_dir.resolve()
+    data_directory = args.data_dir.resolve()
+
+
+    # Verify all given paths
+    for label, path in (
+        ("Configuration Directory", config_directory),
+        ("Data Directory", data_directory),
+        ("Emulator Library Directory", emulator_lib)
+    ):
+        if not path.is_dir():
+            parser.error(f"The given {label} either does not exist, or is not a directory (folder). Looked at {path}")
+        if not os.access(path, os.R_OK):
+            parser.error(f"The given {label} is not readable. Looked at {path}")
+
+    if not satellite_main.is_file():
+        parser.error(f"main.py was not found. Tried to look at {path}")
